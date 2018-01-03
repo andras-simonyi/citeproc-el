72178466b01e7f3dad9fb950f3ce6c59b457ae19
András Simonyi <andras.simonyi@gmail.com>
2018-01-03 13:16:49 +0100

    Update release

--- citeproc.el
+++ citeproc.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/andras-simonyi/citeproc-el
 ;; Keywords: bib
 ;; Package-Requires: ((emacs "25") (dash "2.13.0") (s "1.12.0") (f "0.18.0") (queue "0.2") (string-inflection "1.0"))
-;; Version: 0.1.1
+;; Version: 0.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
