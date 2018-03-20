1e2d0fec8e538c0038f90aa794e742c4c56263dc
András Simonyi <andras.simonyi@gmail.com>
2018-03-20 14:53:34 +0100

    Shorten commentary in citeproc.el

--- citeproc.el
+++ citeproc.el
@@ -27,8 +27,7 @@
 ;;; Commentary:
 
 ;; citeproc-el is a library for rendering citations and bibliographies in styles
-;; described in the Citation Style Language (CSL). This file contains the public
-;; API. See the accompanying README for full documentation.
+;; described in the Citation Style Language (CSL).
 
 ;;; Code:
 
