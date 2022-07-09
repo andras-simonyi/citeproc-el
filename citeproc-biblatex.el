392106d5c90b20ac3a4dc0e9054bfbe0ddfc0f3d
András Simonyi <andras.simonyi@gmail.com>
2022-07-09 09:45:44 +0200

    Fix type of the added blt-type entry fields

--- citeproc-biblatex.el
+++ citeproc-biblatex.el
@@ -346,7 +346,7 @@ biblatex variables in B."
 			     (citeproc-bt--to-csl ~reftype)))
 	    result))
     ;; We store the original bib(la)tex type for filtering purposes.
-    (push (cons 'blt-type ~type) result)
+    (push (cons 'blt-type (symbol-name ~type)) result)
     ;; names
     ;; TODO: handle  editorb and editorc as well...
     (when-let ((~editortype (alist-get 'editortype b))
